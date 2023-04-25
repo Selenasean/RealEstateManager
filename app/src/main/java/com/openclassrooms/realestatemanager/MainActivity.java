e0b94af61f1b20d63ac469f83582c85c696cadbb
Nino <anthony@delcey.fr>
2023-04-25 13:46:57 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.realestatemanager;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMain;
+    private TextView textViewQuantity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
+
+        configureTextViewMain();
+        configureTextViewQuantity();
+    }
+
+    private void configureTextViewMain(){
+        textViewMain.setTextSize(15);
+        textViewMain.setText("Le premier bien immobilier enregistré vaut ");
+    }
+
+    private void configureTextViewQuantity(){
+        int quantity = Utils.convertDollarToEuro(100);
+        textViewQuantity.setTextSize(20);
+        textViewQuantity.setText(quantity);
+    }
+}
